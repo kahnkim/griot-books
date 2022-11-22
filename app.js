d2e3c458cfe82d9c5929ce4ccb75911936a7cb4a
Kahner Kim <kahner.kim8@gmail.com>
2022-11-22 16:32:12 -0500

    Set static folder

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
@@ -20,6 +21,8 @@ if(process.env.NODE_ENV === 'development'){
 app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', '.hbs')
 
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.use('/', require('./routes/index'))
