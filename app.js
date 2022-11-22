87966bfc5510208dbcaec23d091d938fa3e09cfe
Kahner Kim <kahner.kim8@gmail.com>
2022-11-22 13:00:14 -0500

    Set up index routes and views

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
+
 // Load config
 dotenv.config({ path: './config/config.env'})
 
@@ -15,8 +17,12 @@ if(process.env.NODE_ENV === 'development'){
 }
 
 // Handlebars
-app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
+app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', '.hbs')
+
+
+// Routes
+app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 5000
 
