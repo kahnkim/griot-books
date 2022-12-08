cf81f87c2c1145b7f9f3ec19f110062cc16691ba
Kahner Kim <kahner.kim8@gmail.com>
2022-12-08 16:14:34 -0500

    Create logout route

--- routes/auth.js
+++ routes/auth.js
@@ -11,4 +11,14 @@ router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
     res.redirect('/dashboard')
 })
+
+// @desc    Logout user
+// @route   /auth/logout
+router.get('/logout', (req, res, next) => {
+    req.logout((error) => {
+        if (error) {return next(error)}
+        res.redirect('/')
+    })
+  })
+
 module.exports = router
