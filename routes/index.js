3df0c5f11d1fd07b4c61cb10747160768d706eee
Kahner Kim <kahner.kim8@gmail.com>
2022-12-19 00:56:53 -0500

    Create views for dashboard reviews

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
+const Review = require('../models/Review')
+
 // @desc    Login/Landing page
 // @route   GET /
 router.get('/', ensureGuest, (req, res) => {
@@ -12,10 +14,16 @@ router.get('/', ensureGuest, (req, res) => {
 
 // @desc    Dashboard
 // @route   GET /dashboard
-router.get('/dashboard', ensureAuth, (req, res) => {
-    res.render('dashboard', {
-        name: req.user.firstName,
-        
-    })
+router.get('/dashboard', ensureAuth, async (req, res) => {
+    try {
+        const reviews = await Review.find({ user: req.user.id }).lean()
+        res.render('dashboard', {
+            name: req.user.firstName,
+            reviews
+        })
+    } catch (err) {
+        console.log(err)
+        res.render('error/500')
+    }
 })
 module.exports = router
